77f067f82145e88f336474bce6d5c14f476972fe
Adrian Amaya <aa17.1atq@gmail.com>
2016-11-09 10:39:38 -0700

    ready for calculator

---
+++ public/js/CalculatorController.ts
@@ -0,0 +1,9 @@
+namespace App {
+  export class CalculatorController{
+      static $inject = [];
+      public title;
+      constructor () {
+        this.title = 'Calculator page'
+        }
+      }
+    }
